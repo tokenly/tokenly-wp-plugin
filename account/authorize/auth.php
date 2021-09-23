9934fd45467f81606a53776653187f0184d49bd4
cryptonaut420 <nrathman@ironcladtech.ca>
2021-09-23 02:54:04 -0700

    Update auth.php

--- account/authorize/auth.php
+++ account/authorize/auth.php
@@ -5,8 +5,8 @@ require_once( $_SERVER[ 'DOCUMENT_ROOT' ] . '/wp-load.php' );
 // require($_SERVER[ 'DOCUMENT_ROOT' ] . '/tokenly/wp-config.php');
 
 $tk_settings_options = get_option( 'tk_settings_option_name' ); // Array of All Options
-$client_id_0 = ($tk_settings_options['client_id_0']) ? $tk_settings_options['client_id_0'] : '1865085475'; // Client ID
-$client_secret_1 = ($tk_settings_options['client_secret_1']) ? $tk_settings_options['client_secret_1'] : 'Ke3LJaXn24mdRScl5AsHP9CxmZoxykf3nm5GgZsI'; // Client Secret
+$client_id_0 = ($tk_settings_options['client_id_0']) ? $tk_settings_options['client_id_0'] : ''; // Client ID
+$client_secret_1 = ($tk_settings_options['client_secret_1']) ? $tk_settings_options['client_secret_1'] : ''; // Client Secret
 
 
 
@@ -17,7 +17,7 @@ $_SESSION['user_state'] = $state;
 
 $current_web_url =  "https://";;
 $current_web_url .=  $_SERVER['HTTP_HOST'];
-$callback_url = $current_web_url.'/wp-content/plugins/tokenpass/account/authorize/callback.php';
+$callback_url = $current_web_url.'/wp-content/plugins/tokenly-wp-plugin-main/account/authorize/callback.php';
 
 wp_redirect('https://tokenpass.tokenly.com/oauth/authorize?client_id='.$client_id_0.'&redirect_uri='.$callback_url.'&scope=user,tca&response_type=code&state='.$state.'');
 // header("HTTP/1.1 301 Moved Permanently"); 
