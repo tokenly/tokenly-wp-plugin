81dc1351c8a41a9af3047920f3a718194a146fbb
cryptonaut420 <nrathman@ironcladtech.ca>
2021-09-23 03:19:51 -0700

    Update tokenpass-dashboard.php

--- templates/tokenpass-dashboard.php
+++ templates/tokenpass-dashboard.php
@@ -9,8 +9,7 @@ if(is_user_logged_in()){
 
     $user_id = get_current_user_id();
     $user = wp_get_current_user();
-    // $user_access_token = get_user_meta($user_id, 'access-token', true);
-    $user_access_token ='eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiIxODY1MDg1NDc1IiwianRpIjoiNWUzNzNiZDY1ZWQ4Nzc3MDk5OGE3NGQxM2ZiZDA4ZGNkNjcyNTU4M2Y4NDIyZDNlODU4MGQ3M2NiZTE2NTdlYjBkY2U5NTQ5NDA4ZGQ3OTAiLCJpYXQiOjE2MzIyMzAxMzkuOTY1ODc5LCJuYmYiOjE2MzIyMzAxMzkuOTY1ODgzLCJleHAiOjE2NjM3NjYxMzkuOTU2MTUxLCJzdWIiOiIyMDcwMyIsInNjb3BlcyI6WyJ1c2VyIiwidGNhIl19.DFJQyi7ZXH9JV9aRLucTOC3f6axxyXfHlkqzwsb-ZH_VoCuYvGZ64M3vFK5GyP6QMPQ4R3Z3OUubJQ9yKVtv4TXDdUfhjzxu1jFg7-bmRB3h9OHXyKfAnaU4cwG7GM_5sd55cMkb1g974LNoyaqM4a8KFL32FsyKsaSR0ZJdOYxe3vM1jjS39LB9GjvHh8qGkZW8tUaN8eYXCOU208kLhsTsoo-1JJtdg2kOpnPtyaJLOo-TzWXWCu6R352b2v2lAODTZd9iQgimgmgmqOZAuSPR_daFHkoN3B7D5oMiuvKOHaSx1mzz_4qaWvZQ-C2D5U6ZicoMBCEpxxdiY4X0X903U2faNi9yKLflz4F_GjdfcIr3ZE3yJcHJHZBH4W2oOhX8UTa7P9fN_J4-t69nPjZnu5P83gCNauIs_D1wXDrnXgZ8p--Tm5BPD311yvZdarTLUILqqN7uuKEcRbENzyYU6VYIQ_MIcx7iUonAncu1QrJ3Z_hBEVANlG9xbeZHA08vqTgXZUr_YYwl7_LFasGwWwSxv3QAroxweR53_h5D6s5k6MqWFzylpfU-rxA886AxNeRhj4ifcz8VCfMNcLfF5FGdmANvE8a0WkY4valL0PST2MyBaftj7F2FT8VIQ6bePqatnHb7FOJXS9-EWKU-5Rwaa347U6DFA_16ohE';
+    $user_access_token = get_user_meta($user_id, 'access-token', true);
     $user_data = $user->data;
   // echo"<pre> user_access_token -> "; print_r($user_access_token); echo "</pre>";
     /** Public Balance API */
