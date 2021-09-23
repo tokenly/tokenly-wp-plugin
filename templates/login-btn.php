3404bf21d86994efa94f398565edb47030479527
cryptonaut420 <nrathman@ironcladtech.ca>
2021-09-23 03:08:37 -0700

    update login button

--- templates/login-btn.php
+++ templates/login-btn.php
@@ -5,7 +5,7 @@ $calc_html = null;
 if(!is_user_logged_in()){
   
   $calc_html = '
-  <a href="'.get_site_url().'/wp-content/plugins/tokenly-wp-plugin-main/account/authorize/auth.php" id="tk-login-btn"> Login with Tokenly </a>';
+  <a href="'.get_site_url().'/wp-content/plugins/tokenly-wp-plugin-main/account/authorize/auth.php" id="tk-login-btn" class="btn btn-lg btn-primary">Login with Tokenpass</a>';
 
 }
 
