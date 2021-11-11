b97a103f4fe995faa32a8746c16c01c4186b218e
encore-by-dev <dev@encore.by>
2021-11-11 18:53:41 +0300

    source registration simplified

--- app/Models/Address.php
+++ app/Models/Address.php
@@ -5,5 +5,8 @@ namespace Tokenly\Wp\Models;
 use Tokenly\Wp\Interfaces\Models\AddressInterface;
 
 class Address implements AddressInterface {
-	//
+	address	string	Bitcoin address
+	balances	array	Array of token balances in format Asset => Quantity
+	public	boolean	If this address is set to public or not
+	label	string	Optional display label
 }
