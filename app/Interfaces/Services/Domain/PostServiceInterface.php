fe124d5d23fefae5b3305c052e7988f5ad73a5bf
encore-by-dev <dev@encore.by>
2021-12-07 13:56:20 +0300

    domain services

--- app/Interfaces/Services/Domain/PostServiceInterface.php
+++ app/Interfaces/Services/Domain/PostServiceInterface.php
@@ -2,9 +2,8 @@
 
 namespace Tokenly\Wp\Interfaces\Services\Domain;
 
-use Tokenly\Wp\Interfaces\Collections\TcaRuleCollectionInterface;
-use Tokenly\Wp\Interfaces\Models\UserInterface;
+use Tokenly\Wp\Interfaces\Services\Domain\DomainServiceInterface;
 
-interface PostServiceInterface {
-	public function show( array $params = array() );
+interface PostServiceInterface extends DomainServiceInterface {
+	//
 }
