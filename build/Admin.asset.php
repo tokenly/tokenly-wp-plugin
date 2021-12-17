2f85ee248d61aa0f9503c17495dcf230c3f16690
encore-by-dev <dev@encore.by>
2021-12-17 15:52:10 +0300

    build no longer commited

--- build/Admin.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'wp-components', 'wp-element', 'wp-polyfill'), 'version' => 'cb21a1465e0ec9224a64ab5c8213acd1');
--- build/Frontend.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('wp-polyfill'), 'version' => '2428c9e4dab59ca0ff21ac357daa08db');
