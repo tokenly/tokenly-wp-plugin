f81e8763159acfed66820451c2ac26541386892e
encore-by-dev <dev@encore.by>
2022-01-10 20:00:53 +0300

    user credit listing

--- app/Controllers/Api/User/Credit/BalanceController.php
+++ app/Controllers/Api/User/Credit/BalanceController.php
@@ -2,23 +2,15 @@
 
 namespace Tokenly\Wp\Controllers\Api\User\Credit;
 
-use Tokenly\Wp\Controllers\Controller;
+use Tokenly\Wp\Controllers\Api\User\Controller;
 use Tokenly\Wp\Interfaces\Controllers\Api\User\Credit\BalanceControllerInterface;
 
-use Tokenly\Wp\Interfaces\Services\Domain\UserServiceInterface;
 use Tokenly\Wp\Interfaces\Models\UserInterface;
 
 /**
  * Defines promise-related endpoints
  */
 class BalanceController extends Controller implements BalanceControllerInterface {
-	protected $user_service;
-
-	public function __construct(
-		UserServiceInterface $user_service
-	) {
-		$this->user_service = $user_service;
-	}
 	
 	/**
 	 * Gets a collection of balance
@@ -26,8 +18,8 @@ class BalanceController extends Controller implements BalanceControllerInterface
 	 * @param \WP_REST_Request $request Request data
 	 * @return array
 	 */
-	public function index( UserInterace $user, \WP_REST_Request $request ) {
-		//
+	public function index( UserInterface $user, \WP_REST_Request $request ) {
+		
 	}
 
 	/**
@@ -35,12 +27,9 @@ class BalanceController extends Controller implements BalanceControllerInterface
 	 * @return array
 	 */
 	protected function get_bind_params() {
-		return array(
-			'service'                   => $this->user_service,
-			'single_methods'            => array( 'show' ),
-			'single_service_method'     => 'show',
-			'collection_methods'        => array( 'index' ),
-			'collection_service_method' => 'index',
-		);
+		$params = array_merge( parent::get_bind_params(), array(
+			'single_methods' => array( 'index' ),
+		) );
+		return $params;
 	}
 }
